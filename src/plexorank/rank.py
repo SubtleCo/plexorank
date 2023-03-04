4150e4f18760e8584be30ef964a95111312e039e
Alex Martin <alex.martin@getbuilt.com>
2023-03-04 15:07:30 -0600

    initial commit

---
+++ src/plexorank/rank.py
@@ -0,0 +1,75 @@
+
+"""Functions around generating and modifying elements utilizing a lexicographical ranking system"""
+from math import floor
+
+from utils import (
+    convert_to_base10,
+    convert_to_base26,
+    decipher_rank,
+    find_mean,
+    get_greater_length,
+    normalize_rank,
+    recipher,
+)
+
+LOWEST_RANK = "aaaaaa"
+LOWEST_RANK_VALUE = 0
+HIGHEST_RANK = "eeeeee"
+HIGHEST_RANK_VALUE = 49426524
+INITIAL_CIPHER_LENGTH = 6
+
+
+def create_bulk_ranks(count: int):
+    ordered_ranks = [LOWEST_RANK]
+    if count > 1:
+        interval = floor(HIGHEST_RANK_VALUE / (count - 1))
+        for i in range(count - 1):
+            numerical_rank = interval * (i + 1)
+            base26_rank = convert_to_base26(numerical_rank, INITIAL_CIPHER_LENGTH)
+            rank = recipher(base26_rank)
+            ordered_ranks.append(rank)
+    return ordered_ranks
+
+
+def create_mean_rank(prev_rank: str, next_rank: str) -> str:
+    """Generate a new rank inbetween two given ranks"""
+    cipher_length = get_greater_length(prev_rank, next_rank)
+
+    low_deciphered = decipher_rank(prev_rank)
+    low_normalized = normalize_rank(low_deciphered, cipher_length)
+    low_base10 = convert_to_base10(low_normalized)
+
+    high_deciphered = decipher_rank(next_rank)
+    high_normalized = normalize_rank(high_deciphered, cipher_length)
+    high_base10 = convert_to_base10(high_normalized)
+
+    mean = find_mean(low_base10, high_base10)
+    print(f"{low_base10=}")
+    print(f"{high_base10=}")
+    print(f"{mean=}")
+    mean_base26 = convert_to_base26(mean, cipher_length)
+    new_rank = recipher(mean_base26)
+    print(f"{mean_base26=}")
+
+    return new_rank
+
+
+def create_highest_rank():
+    pass
+
+
+# """
+# OK stanley, here's the planley:
+#     if prev:
+#         find next rank
+#         if next:
+#             generate between prev and next
+#          else: (this is the last rank)
+#              new = rank + 1000
+#      else (no prev):
+#          try 'aaaaaa'
+#          except:
+#              find next
+#              old top = mean(aaaaaa, next)
+#              new rank = aaaaaa
+# """
---
+++ src/plexorank/utils.py
@@ -0,0 +1,64 @@
+"""Utility functions for lexicographical ranking"""
+from math import floor
+
+from cipher_tables import decipher_table, cipher_table
+
+
+def find_mean(high, low):
+    """Find the mean between two base10 numbers"""
+    floored_mean = floor((high + low) / 2)
+    return floored_mean
+
+
+def decipher_rank(rank: str) -> list[int]:
+    """Convert a base26 string rank to a list of numbers"""
+    return [decipher_table[item] for item in rank]
+
+
+def normalize_rank(deciphered: list[int], cipher_length: int) -> list[int]:
+    """Pad a list of numbers with zeros to ensure equal depth comparison"""
+    print(f"{deciphered=}")
+    print(f"{cipher_length=}")
+    while len(deciphered) < cipher_length:
+        deciphered.append[0]
+    return deciphered
+
+
+def convert_to_base10(normalized: str) -> int:
+    """Convert a base26 list of integers to a base10 integer"""
+    numerical_rank = 0
+    for i, number in enumerate(normalized):
+        n = len(normalized) - i - 1
+        x = number * 26**n
+        numerical_rank += x
+    return numerical_rank
+
+
+def convert_to_base26(number: int, cipher_length: int) -> list[int]:
+    """Convert a single base10 integer to a base26 list of ints"""
+    rebased_number = []
+    n = cipher_length - 1
+    for _ in range(cipher_length):
+        x = floor(number / 26**n % 26)
+        rebased_number.append(x)
+        n -= 1
+    return rebased_number
+
+
+def recipher(rebased_number: list[int]) -> str:
+    rank_elements = []
+    for number in rebased_number:
+        element = cipher_table[number]
+        rank_elements.append(element)
+    return "".join(rank_elements)
+
+
+def validate_rank(low, high, new):
+    """Ensure the new rank is available. If not, tack on 'n'"""
+    if new not in (low, high):
+        return new
+    return new + "n"
+
+
+def get_greater_length(a: str, b: str):
+    return len(a) if len(a) >= len(b) else len(b)
